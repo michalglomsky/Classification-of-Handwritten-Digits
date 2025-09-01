a9b864883d238cbb686031d13c82d1f32d070803
Michal Glomski <132297566+michalglomsky@users.noreply.github.com>
2025-09-01 16:21:19 +0200

    Update analysis.py
    STAGE 2 Objectives:
    
    Import a necessary tool from sklearn;
    Use the first 6000 rows of the datasets. Set the test set size as 0.3 and the random seed of 40 to make your output reproducible;
    Print new datasets' shapes.
    Let's make sure that our dataset is balanced after splitting. Print the proportions of samples per class in the training set as in the example below. We recommend using pd.Series.value_counts(normalize=True).
    The input of train-test splitting function is two arrays: features and target. The output contains four arrays stored in the following variables: x_train — the features' train set, x_test — the features' test set, y_train — a target variable from the train set, y_test — a target variable from the test set.
    
    Provide the answers to the questions in the format as shown below.
    
    EXAMPLE:
    
    x_train shape: (100, 5)
    x_test shape: (20, 5)
    y_train shape: (100,)
    y_test shape: (20,)
    Proportion of samples per class in train set:
    1    0.05
    7    0.30
    4    0.25
    6    0.04
    0    0.08
    9    0.01
    3    0.13
    2    0.10
    8    0.01
    5    0.03
    dtype: float64

--- analysis.py
+++ analysis.py
@@ -1,19 +1,30 @@
 import tensorflow
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 import keras
 
 # Load the data
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+x_train, y_train = keras.datasets.mnist.load_data(path='mnist.npz')[0]
 
-# Change shape of the features array to 2D-array of rows for images and columns for pixels
+# Reshape the datasets
 x_train = np.reshape(x_train,newshape=(60000, 784))
+
+# Use only the first 6000 rows
+x_train = x_train[:6000]
+y_train = y_train[:6000]
+
+# Train, test, split
+x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.3, random_state=42)
 
 def main():
     # Print overview of the data
-    print(f"Classes: {np.unique(y_train)}")
-    print(f"Features' shape: {x_train.shape}")
-    print(f"Target's shape: {y_train.shape}")
-    print(f"min: {np.min(x_train)}, max: {np.max(x_train)}")
+    print(f"x_train shape: {x_train.shape}")
+    print(f"x_test shape: {x_test.shape}")
+    print(f"y_train shape: {y_train.shape}")
+    print(f"y_test shape: {y_test.shape}")
+    print("Proportion of samples per class in train set:")
+    print(pd.Series(y_train).value_counts(normalize=True))
 
 if __name__=="__main__":
     main()
