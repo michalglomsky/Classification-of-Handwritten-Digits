d94fe4b9e1bd75169df386eb4db51fe995f241ae
Michal Glomski <132297566+michalglomsky@users.noreply.github.com>
2025-09-05 16:32:14 +0200

    Update analysis.py
    STAGE 5 (FINAL):
    
    DESCRIPTION:
    
    In the final stage, you need to improve model performance by tuning the hyperparameters. No need to do it manually, as sklearn has convenient tools for this task. We advise using GridSearchCV. It searches through the specified parameter values for an estimator. Basically, it takes estimator, param_grid, and scoring as arguments. You can read about them in the documentation. As a starting point, you will be provided with a list of parameters to find a better set than the default one.
    
    We urge you to try more parameter values to improve the result. The test system has minimum requirements on the algorithms and their accuracies. It can pass only two algorithms (K-nearest Neighbors and Random Forest) that performed in the best possible way in the previous stage. Concerning the scores, the test system requires values that can be achieved by finding the best set of parameters from the lists below.
    
    Are you the one with the highest accuracy? Share your scores and the most efficient sets of parameters in the comments!
    
    OBJECTIVES:
    
    Choose a data representation that performed the best. You need to choose between the initial dataset and the one with normalized features. You also take only two models with the highest accuracy;
    Initialize GridSearchCV(estimator=..., param_grid=..., scoring='accuracy', n_jobs=-1) to search over the following parameters:
    For the K-nearest Neighbors classifier: {'n_neighbors': [3, 4], 'weights': ['uniform', 'distance'], 'algorithm': ['auto', 'brute']}
    For the Random Forest classifier: {'n_estimators': [300, 500], 'max_features': ['sqrt', 'log2'], 'class_weight': ['balanced', 'balanced_subsample']}. Don't forget to set random_state=40 in the Random Forest classifier!
    Note that njobs parameter is responsible for the number of jobs that will be run in parallel. Set njobs=-1 to use all processors;
    
    Run the fit method for GridSearchCV. Use the train set only. Since a number of models (one of two algorithms with a set of parameter values is one model) must be trained to compare the performances, this step will take about 30 minutes;
    Print the best sets of parameters for both algorithms. You can get this information in the attribute called best_estimator_ of each algorithm's GridSearchCV instance. Train two best estimators on the test set and print their accuracies.
    The input includes the train and test sets. The output consists of two classifiers that perform better than the ones from the previous stage. Provide the answer in the format shown below. The values are given for reference only, the actual numbers may be different.
    
    EXAMPLE OUTPUT:
    
    K-nearest neighbours algorithm
    best estimator: KNeighborsClassifier(n_neighbors=5, weights='uniform')
    accuracy: 1
    
    Random forest algorithm
    best estimator: RandomForestClassifier(class_weight=None, max_features='sqrt',
                           n_estimators=100, random_state=40)
    accuracy: 1

--- analysis.py
+++ analysis.py
@@ -8,6 +8,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import precision_score, f1_score
 from sklearn.preprocessing import Normalizer
+from sklearn.model_selection import GridSearchCV
 import keras
 
 # Load the data
@@ -30,64 +31,40 @@ x_test_norm = normalizer.fit_transform(x_test)
 
 
 # Function for fitting, predicting and evaluating a model
-def fit_predict_eval(model, features_train, features_test, target_train, target_test, scores_dict):
+def fit_predict_eval(model, features_train, features_test, target_train, target_test):
     # here you fit the model
     model.fit(features_train, target_train)
 
-    # make a prediction
+    # Make a prediction
     target_pred = model.predict(features_test)
-    # The next line is not used, so it's commented out to clean the code.
-    # target_proba = model.predict_proba(features_test)
 
-    # calculate accuracy and save it to score
+    # Calculate accuracy and save it to score
     score = precision_score(target_test, target_pred, average='macro', zero_division=0)
 
-    # Print the message
-    print(f'Model: {model}\nAccuracy: {round(score, 4)}\n')
-
     # Store the score in the dictionary
-    scores_dict[str(model.__class__.__name__)] = score
-
+    print(f'best estimator: {model}')
+    print(f'accuracy: {score}\n')
 
 def main():
-    # Dictionary of models
-    models = [
-        KNeighborsClassifier(),
-        DecisionTreeClassifier(random_state=40),
-        LogisticRegression(random_state=40, max_iter=1000),
-        RandomForestClassifier(random_state=40)
-    ]
 
-    # Evaluate each model with normalized data and store scores
-    # Dictionaries for accuracy scores of non-normalized and normalized data
-    scores, scores_norm = {}, {}
+    # Hyperparameters for each model
+    KNN_hyperparameters = {'n_neighbors': [3, 4], 'weights': ['uniform', 'distance'], 'algorithm': ['auto', 'brute']}
+    RForest_hyperparameters = {'n_estimators': [300, 500], 'max_features': ['sqrt', 'log2'], 'class_weight': ['balanced', 'balanced_subsample']}
 
-    print("Non-normalized data scores:\n")
-    for model in models:
-        fit_predict_eval(model, x_train, x_test, y_train, y_test, scores)
+    # Grid search for each model
+    grid_search_KNN = GridSearchCV(estimator=KNeighborsClassifier(), param_grid=KNN_hyperparameters, scoring='accuracy', n_jobs=-1)
+    grid_search_RForest = GridSearchCV(estimator=RandomForestClassifier(random_state=40), param_grid=RForest_hyperparameters, scoring='accuracy', n_jobs=-1)
 
-    print("Normalized data scores:\n")
-    for model in models:
-        fit_predict_eval(model, x_train_norm, x_test_norm, y_train, y_test, scores_norm)
-
-    # Check whether normalization have a net positive impact on the accuracy scores
-    comparison = 0
-    for score, score_norm in scores, scores_norm:
-        comparison += score_norm-score
-        
-    # Final message - Q1: Does the normalization have a positive impact in general? (yes/no)
-    if comparison > 0:
-        print(f"\nThe answer to the 1st question: yes")
-    else:
-        print(f"\nThe answer to the 1st question: no")
-
-    # Q2: Which two models show the best scores? Round the result to the third decimal
-    #     place and print the accuracy of models in descending order.
-
-    # Sort the normalized scores dictionary in descending order
-    sorted_scores_norm = sorted(scores_norm.items(), key=lambda item: item[1], reverse=True)
-    print(f"The answer to the 2nd question: {sorted_scores_norm[0][0]}-{round(sorted_scores_norm[0][1], 3)}, {sorted_scores_norm[1][0]}-{round(sorted_scores_norm[1][1], 3)}")
-
+    # Dictionary of models for cross-validation
+    models = {'K-nearest neighbours algorithm': grid_search_KNN,
+        'Random forest algorithm': grid_search_RForest}
+    
+    # Running through models on normalized data
+    for model in models.items():
+        print(f'{model[0]}')
+        model[1].fit(x_train_norm, y_train)
+        best_model = model[1].best_estimator_
+        fit_predict_eval(best_model, x_train_norm, x_test_norm, y_train, y_test)
 
 if __name__ == "__main__":
     main()
